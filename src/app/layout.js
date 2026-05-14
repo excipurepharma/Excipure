0540c309ba506d5e1e23d81de218517deff44214
excipurepharma <excipurepharma@gmail.com>
2026-05-14 18:47:32 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
-import './globals.css'
-import { CartProvider } from '../context/CartContext'
+import './globals.css'; // Path must be exact!
+import { CartProvider } from '../context/CartContext';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="antialiased">
         <CartProvider>
           {children}
         </CartProvider>
       </body>
     </html>
-  )
+  );
 }
